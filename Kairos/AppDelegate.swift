b2509e80f62c5fb9b7c8ee9d70d03bb6717878a4
Gregory Berns-Leone <gberns.l@gmail.com>
2016-05-15 16:02:56 -0500

    Add popover toggle on button press and view controller file

--- Kairos/AppDelegate.swift
+++ Kairos/AppDelegate.swift
@@ -14,20 +14,37 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var window: NSWindow!
 
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSSquareStatusItemLength)
+    let popover = NSPopover()
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         if let button = statusItem.button {
             button.image = NSImage(named: "AlarmClock_black")
             button.action = #selector(AppDelegate.togglePopover(_:))
         }
+        
+        popover.contentViewController = TimerSelectViewController(nibName: "TimerSelectViewController", bundle: nil)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
 
+    func showPopover(sender: AnyObject?) {
+        if let button = statusItem.button {
+            popover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: NSRectEdge.MinY)
+        }
+    }
+    
+    func hidePopover(sender: AnyObject?) {
+        popover.performClose(sender)
+    }
+    
     func togglePopover(sender: AnyObject) {
-        print("togglePopover")
+        if popover.shown {
+            self.hidePopover(sender)
+        } else {
+            self.showPopover(sender)
+        }
     }
 }
 
---
+++ Kairos/TimerSelectViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TimerSelectViewController.swift
+//  Kairos
+//
+//  Created by Gregory Berns-Leone on 5/15/16.
+//  Copyright © 2016 GregApps. All rights reserved.
+//
+
+import Cocoa
+
+class TimerSelectViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
